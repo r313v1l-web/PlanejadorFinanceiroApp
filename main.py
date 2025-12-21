d86810ec1bb90e9b8536808bb10dc81ac88a115f
r313v1l-web <r313v1l@gmail.com>
2025-12-21 20:46:51 -0300

    MIGRACAO PARA O SUPA

--- main.py
+++ main.py
@@ -12,7 +12,17 @@ import bcrypt
 
 
 
+import bcrypt
 
+teste_hash = "$2b$12$9yPp2K4z9l0y5Hnq1gC0QeZxD5jP1d2fH9sZQjFZ9Kp0J3WcXyV5y"
+
+st.write(
+    "TESTE BCRYPT:",
+    bcrypt.checkpw(
+        b"admin123",
+        teste_hash.encode("utf-8")
+    )
+)
 
 # =========================================================
 # AUTENTICAÇÃO
